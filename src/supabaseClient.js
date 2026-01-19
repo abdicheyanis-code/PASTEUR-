4d8424dbf0a54ba2fe921a6dfb0bb98037c8831f
abdicheyanis-code <abdicheyanis@gmail.com>
2026-01-19 20:05:08 +0100

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 // --- REMPLACE CES DEUX LIGNES PAR TES INFOS SUPABASE ---
-const supabaseUrl = 'https://ton-url-ici.supabase.co' 
-const supabaseKey = 'ta-cle-publique-ici'
+const supabaseUrl = mcjgmorvpzsbjlgqptdx
+const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1jamdtb3J2cHpzYmpsZ3FwdGR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg4NDU5MDAsImV4cCI6MjA4NDQyMTkwMH0.4wx4c1LCycvx8BHQ8PQvxXY_Bf48qMD9mZ43k6f8RCo
 // -------------------------------------------------------
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
